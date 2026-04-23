de3964a6d828c9b749458fdac59360f6756d65ac
VatsalParikh0112 <vatsalparikh0112@gmail.com>
2026-04-23 20:54:34 +0530

    feat: set up common routes and components for the application

---
+++ src/app/routes/common/follow-page/follow-page.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FollowPage } from './follow-page';
+
+describe('FollowPage', () => {
+  let component: FollowPage;
+  let fixture: ComponentFixture<FollowPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FollowPage],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FollowPage);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
